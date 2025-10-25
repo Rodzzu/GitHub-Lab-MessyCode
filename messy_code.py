2e3cb6bcb6349fb6b8bb8ca613365d91902e6d2f
Veur31 <tfetesio@gmail.com>
2025-10-25 11:13:28 +0800

    Cleaned up messy code and error handling

--- messy_code.py
+++ messy_code.py
@@ -1,11 +1,17 @@
-#this program add numbers and print the result but its really messy
 
-def addnumb(a,b):return a+b
-def main( ):
-  print("This is a simple adder program")
-  a=input("enter first number")
-  b=input("enter second number")
-  res=addnumb(int(a),int(b))
-  print("the sum is :",res)
-main( )#call the function at end
-# KELVIN CODE
+#Function to add two numbers
+def addnumb(a,b):
+  try: #Implement Error handling
+    return int(a + b)
+  except ValueError:
+    print("Please Input Valid numbers")
+
+  #Function to excecute the program
+def main():
+  firstNumber=input(("enter first number: "))
+  secondNumber=input(("enter second number: "))
+  result=addnumb(firstNumber, secondNumber)
+  print(f"the sum is : {result}")
+
+print("This is a simple adder program")
+main()
